7ae9cd2c8efbfe0522e5956fa124baf81320e6ec
b0und <f1arenova1@gmail.com>
2021-04-19 23:29:24 +0300

    scrape cron path change

--- cron_scrape.sh
+++ cron_scrape.sh
@@ -4,13 +4,13 @@
 # crontab -e
 # 00 06 * * * bash /home/ubuntu/kotanima_scraper/cron_scrape.sh >> /home/ubuntu/kotanima_scraper/scrape_log.txt 2>&1
 
-cd /home/kotanima_project/kotanima_scraper/
-poetry run python /home/kotanima_project/kotanima_scraper/yandex_backup.py
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="patchuu" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="awenime" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="moescape" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="fantasymoe" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="awwnime" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="artistic_ecchi" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/scrape_reddit.py praw_scrape --subreddit_name="ecchi" --amount=1000 --PRAW_MODE=PostSearchType.NEW
-poetry run python /home/kotanima_project/kotanima_scraper/yandex_backup.py
+cd /home/kotanima_project/kotanima_content/
+poetry run python /home/kotanima_project/kotanima_content/yandex_backup.py
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="patchuu" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="awenime" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="moescape" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="fantasymoe" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="awwnime" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="artistic_ecchi" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/scrape_reddit.py praw_scrape --subreddit_name="ecchi" --amount=1000 --PRAW_MODE=PostSearchType.NEW
+poetry run python /home/kotanima_project/kotanima_content/yandex_backup.py
