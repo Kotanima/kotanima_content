63b90962ffa51803df3adb5d832124a4e608cad8
b0und <f1arenova1@gmail.com>
2021-04-20 12:38:42 +0300

    rewrite sh scripts

--- cron_downloader.sh
+++ cron_downloader.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/kotanima_project/kotanima_content/
+
+# 0 * * * * bash /home/kotanima_project/kotanima_content/cron_scrape.sh >> /home/ubuntu/kotanima_scraper/scrape_log.txt 2>&1
+
+source /home/kotanima_project/kotanima_content/.venv/bin/activate
 cd /home/kotanima_project/kotanima_content
-source $(poetry env info --path)/bin/activate
 python src/downloader.py
