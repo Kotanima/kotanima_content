03a0ac59d6974b312159953a09a6b1f4a7274742
b0und <f1arenova1@gmail.com>
2021-06-04 14:05:37 +0300

    attempt to fix downloader sql

--- src/downloader.py
+++ src/downloader.py
@@ -46,10 +46,10 @@ def get_reddit_post_data(cursor, limit: int):
     # TODO get safe subs from server
     query = f"""SELECT post_id, author, created_utc, title, url, phash, sub_name FROM my_app_redditpost 
               WHERE sub_name IN ('awwnime','fantasymoe','patchuu','awenime','moescape')
-              AND phash NOT IN (SELECT phash FROM my_app_vkpost)
-              AND phash NOT IN (SELECT phash FROM my_app_redditpost where selected=false)
               AND wrong_format=false
               AND selected is NULL
+              AND phash NOT IN (SELECT phash FROM my_app_vkpost)
+              AND phash NOT IN (SELECT phash FROM my_app_redditpost where selected=false)
               ORDER BY created_utc DESC
               LIMIT {limit}"""
 
