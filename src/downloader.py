efc050bd23af036f5f377c862dbaabc608abeadf
b0und <f1arenova1@gmail.com>
2021-06-06 23:01:30 +0300

    dl bugs

--- src/downloader.py
+++ src/downloader.py
@@ -49,8 +49,7 @@ class RedditPost:
 def get_reddit_post_data(cursor, limit: int):
     # TODO get safe subs from server
     query = f"""SELECT post_id, author, created_utc, title, url, phash, sub_name FROM my_app_redditpost 
-              WHERE selected IS NOT FALSE
-              AND selected IS NOT TRUE
+              WHERE selected IS NULL
               AND wrong_format IS FALSE
               AND sub_name IN ('awwnime','fantasymoe','patchuu','awenime','moescape')
               AND phash NOT IN (SELECT phash FROM my_app_vkpost)
