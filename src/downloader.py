bd606d358fadd7fc4ea45abb772539f4890f8c74
Unbound <f1arenova1@gmail.com>
2021-07-16 00:58:18 +0300

    dlt

--- src/downloader.py
+++ src/downloader.py
@@ -65,16 +65,15 @@ def get_reddit_post_data(cursor, limit: int):
               LIMIT {limit}"""
 
     query = f"""SELECT post_id, author, created_utc, title, url, phash, sub_name FROM my_app_redditpost 
-              WHERE (NOT(selected IS NOT NULL))
-              AND phash NOT IN (SELECT phash FROM my_app_vkpost)
-              AND sub_name IN ('awwnime','fantasymoe','patchuu','awenime','moescape')
-              AND wrong_format IS NOT TRUE
-              AND dislike IS NOT TRUE
+              WHERE (COALESCE(selected) IS NULL)
+              AND (phash NOT IN (SELECT phash FROM my_app_vkpost))
+              AND (sub_name IN ('awwnime','fantasymoe','patchuu','awenime','moescape'))
+              AND (wrong_format IS NOT TRUE)
+              AND (dislike IS NOT TRUE)
               ORDER BY created_utc DESC
               LIMIT {limit}"""
 
 
-    print(cursor.mogrify(query))
 
     try:
         cursor.execute(query)
