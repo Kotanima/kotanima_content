4a5098f35e0495b81035e9e9741a2acd9f33e1ce
Unbound <f1arenova1@gmail.com>
2021-07-21 20:25:52 +0300

    sh scripts cleanup

--- sh_scripts/restore_db.sh
+++ sh_scripts/restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # example useage:
 # ./restore_db.sh 2021-07-16.backup
-pg_restore --clean --dbname postgres --host localhost --port 5432 --username postgres --no-owner "$1"
+pg_restore --clean --if-exists  --host=localhost --port=5432 --username=postgres --no-owner --create --dbname=kaa_prod "$1"
